50113179adeef85a7264ab5029feee962556084a
tolymhlv <anatolij.m@baltinfocom.ru>
2021-01-30 14:17:29 +0300

    Add client

--- src/server/DataBase.js
+++ src/server/DataBase.js
@@ -12,7 +12,7 @@ const dotenv = require('dotenv');
 const pg = require('pg');
 dotenv.config();
 const isProduction = process.env.NODE_ENV;
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_USER}`;
+const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const pool = new pg.Pool({
     connectionString: isProduction
         ? process.env.DATABASE_URL
